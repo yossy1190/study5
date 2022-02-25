f33911e1efc9fbe1b3df0fda7a8a2759bdb2306d
yossy1190 <69608729+yossy1190@users.noreply.github.com>
2022-02-26 07:02:57 +0900

    Update possystem.py

--- possystem.py
+++ possystem.py
@@ -15,8 +15,6 @@ class Item:
         self.item_name=item_name
         self.price=price
 
-
-
 ### オーダークラス
 class Order:
     # item_mastersの中には、CSVで読み込んだ、code,name,priceが入る
@@ -31,13 +29,16 @@ class Order:
         self.pay_money=0
         self.change_money=0
 
-    def add_order_list(self,item_code):          
+    def add_order_list(self,item_code):
+           
         for item_master in self.item_masters:
-            if item_code in item_master.item_code:
-                return item_code
+            if int(item_code) == int(item_master.item_code):
+                return item_master.item_code,item_master.item_name,item_master.price
+        
             else:
-                return "error"
+                return "Error"
             
+    '''機能追加用メソッド
     def print_order_item(self,val_item_code,amount):
             # item_mastersから１つのitemを取り出し、item_code,item_name,priceにアクセスできるようにする。
             # 入力されたitem_codeがitem_mastersに含まれているか確認。
@@ -49,15 +50,16 @@ class Order:
                     self.subtotal = int(item.price)*int(amount)
                     print(f"商品コード:{item.item_code},商品名:{item.item_name},価格:{int(item.price):,}円,個数{amount}個")
             print(f"小計:{int(self.subtotal):,}円")
-            
-        
+    '''       
+    '''機能追加用メソッド  
     def pay_total_money(self):
         
         self.pay_money=int(input(f"お会計は{int(self.alltotal):,}円です。預け入れ金額を入力してください(円不要)。>>>"))
         self.change_money=self.pay_money-int(self.alltotal)
         print(f"お支払いありがとうございます。お釣りは{self.change_money:,}円です。本日はご来店ありがとうございました!")
        
-    
+    '''
+    '''機能追加用メソッド
     def print_receipt(self):
         payment_date=datetime.datetime.now().strftime("%Y/%m/%d %H:%M:%S")
         
@@ -69,3 +71,4 @@ class Order:
             f.write(f"合計金額:{int(self.alltotal):,}円\nお預かり金額:{int(self.pay_money):,}円\n")
             f.write(f"お釣り:{int(self.change_money):,}円\n")
             f.write("本日はご来店ありがとうございました！")
+    '''   
