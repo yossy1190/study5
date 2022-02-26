793ebc9555bcaed59295c052aaf1efea4a0cba1f
yossy1190 <69608729+yossy1190@users.noreply.github.com>
2022-02-26 13:45:15 +0900

    Update view.py

--- view.py
+++ view.py
@@ -34,20 +34,38 @@ for item_in in items_in:
 '''
 「カートに入れる」処理
 '''
-# orders=[]
-# for item_master in item_masters:
-#     orders.append(pos.Order(item_master))
-# ordersにはOrderクラスが１つずつ入っている状態
-
 order=pos.Order(item_masters)
+alltotal=0
+subtotal=0
 
 @eel.expose
-def add_order_list(item_code):
-  
+def add_order_list(item_code,item_amount):
+    # print(order.item_masters)
     item_infos= order.add_order_list(item_code)
-    return f"商品コード{item_infos[0]}、商品名{item_infos[1]}、商品価格{item_infos[2]}円をカートに入れました"
+    if item_infos:
+        global alltotal
+        global subtotal
+        alltotal+=int(item_infos[2])*int(item_amount)
+        subtotal=int(item_infos[2])*int(item_amount)
+        return f"商品コード{item_infos[0]}、商品名{item_infos[1]}、商品価格{int(item_infos[2]):,}円、{item_amount}個をカートに入れました。小計:{subtotal:,}円 合計:{alltotal:,}円です。"
+    else :
+        return f"[エラー] 商品コード{item_code}が見つかりませんでした。"
 
-    
+'''
+買物終了時の処理
+'''
+@eel.expose
+def fin_shop():
+    return f"お買上金額は、{alltotal:,}円です。\n支払金額スペースに金額を入力した後、「支払」ボタンを押してください。"
+
+
+'''
+金額支払時の処理
+'''
+@eel.expose
+def cul_shop(depo_money):
+    change_money=int(depo_money)-int(alltotal) 
+    return f"お買上金額:{int(alltotal):,}円 お支払金額:{int(depo_money):,}円 お釣り:{int(change_money):,}円です。ご購入ありがとうございました。"
 
 desktop.start(app_name,end_point,size)
 
